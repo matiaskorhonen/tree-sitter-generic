4a967c7ca3ce6116827ddf090eaa4ef7d10c03df
Matias Korhonen <me@matiaskorhonen.fi>
2026-03-17 21:37:12 +0200

    Hardcode sources

--- Package.swift
+++ Package.swift
@@ -1,27 +1,26 @@
 // swift-tools-version:5.3
 
-import Foundation
 import PackageDescription
-
-var sources = ["src/parser.c"]
-if FileManager.default.fileExists(atPath: "src/scanner.c") {
-    sources.append("src/scanner.c")
-}
 
 let package = Package(
     name: "TreeSitterGeneric",
     products: [
-        .library(name: "TreeSitterGeneric", targets: ["TreeSitterGeneric"]),
+        .library(name: "TreeSitterGeneric", targets: ["TreeSitterGeneric"])
     ],
     dependencies: [
-        .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
+        .package(
+            name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter",
+            from: "0.9.0")
     ],
     targets: [
         .target(
             name: "TreeSitterGeneric",
             dependencies: [],
             path: ".",
-            sources: sources,
+            sources: [
+                "src/parser.c",
+                "src/scanner.c",
+            ],
             resources: [
                 .copy("queries")
             ],
@@ -35,7 +34,7 @@ let package = Package(
                 "TreeSitterGeneric",
             ],
             path: "bindings/swift/TreeSitterGenericTests"
-        )
+        ),
     ],
     cLanguageStandard: .c11
 )
